bda7d2ac80d5141267e91873e6a63a924fb0c1dd
Václav Šmilauer <eu@doxos.eu>
2022-06-13 14:45:54 +0200

    wondows wheel build; do cleanups and bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from skbuild import setup
 import sys
 
-if sys.platform=='win32':
-    cmake_args=['-G','Visual Studio 16 2019']
+# this is necessary, otherwise skbuild tried Ninja first which picks up MSVC but then
+# tries to configure Eigen with gfortran.exe with MSVC flags and fails. This forces
+# native MSVC generator for CMake which won't fint gfortran.exe at all
+if sys.platform=='win32': cmake_args=['-G','Visual Studio 16 2019']
 else: cmake_args=[]
 
 setup(
     name="mupif-accel",
-    version="0.0.1",
+    version="0.0.2",
     description="Optional accelerated components for MuPIF",
     author='Václav Šmilauer',
     license="LGPLv3",
